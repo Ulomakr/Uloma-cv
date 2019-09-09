379bb8fbb27b0a8ecff53727115639b257128ff6
David <ulomakr@gmail.com>
2019-09-09 04:11:44 +0100

    first commit

---
+++ response.php
@@ -0,0 +1,11 @@
+<?php
+
+$Name = $_POST['name'];
+$Email = $_POST['email'];
+$Title = $_POST['title'];
+$Message = $_POST['message'];
+
+echo $Name . '<br>';
+echo $Email . '<br>';
+echo $Title . '<br>';
+echo $Message . '<br>';
